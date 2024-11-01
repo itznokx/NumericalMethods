0f673d92a0894dc0bcdd32b70c6abb5eb7bfb4ab
itznokx <dimitri.b.medeiros@gmail.com>
2024-11-01 09:07:50 -0300

    Conversion decimal to N base (N < 36 && N > 1)

--- t1/conversion.hpp
+++ t1/conversion.hpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
+#include <stack>
 using namespace std;
 
-string* decimal_to_nBase (int n,int base){
-	vector<int> aux;
+string decimal_to_nBase (int n,int base){
+	if (base > 36 || base < 1){
+		return "Invalid base number.\n";
+	}
+	string alfabeth [36] = {"0","1","2","3","4","5","6","7","8","9",
+							"A","B","C","D","E","F","G","H","I","J",
+							"K","L","M","N","O","P","Q","R","S","T",
+							"U","V","W","X","Y","Z"};
+	bool isNegative;
+	string final;
+	if (n > 0){
+		isNegative = false;
+	}
+	else {
+		isNegative = true;
+		final += "-";
+	}
+	stack<int> aux;
 	while (n > base){
-		aux.push_back(n%base);
+		aux.push(n%base);
 		n = n/base;
 	}
-	aux.push_back(n);
+	aux.push(n);
 	int x = 0;
-	string* final = new string[(sizeof(aux)/sizeof(int))+1];
-	for (const int& i : aux){
-		final[x] = to_string(i);
+	while (!aux.empty()){
+		int top = aux.top();
+		if (top < 10){
+			final += to_string(top);
+		}
+		else{
+			final += alfabeth[top];
+		}
+		aux.pop();
 	}
 	return final;
 }
