4ad4a6cd14630212e58fc97fbf1cf0e5aac606f1
itznokx <dimitri.b.medeiros@gmail.com>
2024-11-01 09:50:36 -0300

    Conversion nbase to decimal (UNFIXED)

--- t1/conversion.hpp
+++ t1/conversion.hpp
@@ -2,7 +2,13 @@
 #include <cmath>
 #include <stack>
 using namespace std;
-
+int returnIndex(string f,string array[]){
+	for (int i=0;i<array->size();i++){
+		if (f == array[i])
+			return i;
+	}
+	return -1;
+}
 string decimal_to_nBase (int n,int base){
 	if (base > 36 || base < 1){
 		return "Invalid base number.\n";
@@ -38,4 +44,30 @@ string decimal_to_nBase (int n,int base){
 		aux.pop();
 	}
 	return final;
+}
+string nBase_to_decimal (string n,int base){
+	if (base > 36 || base < 1){
+		return "Invalid base number.\n";
+	}
+	string alfabeth [36] = {"0","1","2","3","4","5","6","7","8","9",
+							"A","B","C","D","E","F","G","H","I","J",
+							"K","L","M","N","O","P","Q","R","S","T",
+							"U","V","W","X","Y","Z"};
+	int final;
+	bool isNegative = false;
+	if (to_string(n[0]) == "-"){
+		isNegative = true;
+		for (int i=1;i<n.length();i++){
+			final += (pow(base,i-1))*returnIndex(to_string(n[i]),alfabeth);
+		}
+		final = final * (-1);
+	}
+	else {	
+		for (int i=0;i<n.length();i++){
+			string aux = "";
+			aux += n[i];
+			final += (pow(base,i))*returnIndex(aux,alfabeth);
+		}
+	}
+	return to_string(final);
 }
--- t1/main.cpp
+++ t1/main.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 int main (void){
-	cout << decimal_to_nBase(99887766,36) << "\n";
+	string n1 = decimal_to_nBase(99887766,36);
+	string n2 = nBase_to_decimal(n1,10); 
+	cout << n1 << "\n" << n2 << "\n";
 	return 0;
 }
