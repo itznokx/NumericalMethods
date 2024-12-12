30571743c6745b4eb1840ebce8c9626abf691826
itznokx <dimitri.b.medeiros@gmail.com>
2024-12-12 04:15:17 -0300

    false point method

---
+++ t2/falsepoint.hpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include "function.hpp"
+
+#define MAX_ITER 1000
+double falsepoint (double (*func) (double),double a,double b,double epsilon,int iter){
+	double root;
+	double x,fx;
+	double fa = func(a);
+	double fb = func(b);
+	if (fa*fb > 0){
+		std::cout << "Erro de isolamento entre as raizes A e B";
+		return -404.404;
+	}
+	double intervX = abs(b-a);
+	while (func(a) > epsilon){
+		x = (a*func(b)-b*func(a))/(func(b)-func(a));
+		if (intervX <= epsilon || iter >= MAX_ITER)
+			return x;
+		fx = func(x);
+		printf("iter: %i\na: %f\nb:%f\nx:%f\nf(x): %f\nf(a): %f\nf(b): %f\n|b-a|:%f\n\n",iter,a,b,x,fa,fb,fx,intervX);
+		if (fa*fx > 0){
+			a = x;
+			fa = fx;
+		}else{
+			b = x;
+			fb = x;
+		}
+		iter++;
+		intervX = intervX/2;
+	}
+	root = x;
+	return root;
+}
--- t2/main2.cpp
+++ t2/main2.cpp
@@ -1,6 +1,10 @@
 #include "bisection.hpp"
-
+#include "falsepoint.hpp"
 int main (void){
-	std::cout << bisection(func,0,1,pow(10,-4),0) << "\n";
+	double input;
+
+	//std::cin >> input;std::cout << func(input) << "\n";
+	//std::cout << bisection(func,0,1,pow(10,-4),0) << "\n";
+	std::cout << falsepoint(func,0,1,pow(10,-4),0) << "\n";
 	return 0;
 }
