d6b3af4261147f6bd9efd75b1ce0e16a523ab8ea
itznokx <dimitri.b.medeiros@gmail.com>
2024-12-22 21:50:44 -0300

    t2 qst 1

--- t2/main2_1.cpp
+++ t2/main2_1.cpp
@@ -14,7 +14,7 @@ int main (void){
 	//std::cout << falsepoint(func1,0,1,pow(10,-4),0) << "\n";
 	*/
 	double a0,b0;
-	std::cout << "Function f(a) = -(e/2)+2cos(a)\n";
+	std::cout << "Function f(a) = -(e^{a}/2)+2cos(a)\n";
 	std::cout << "\nMétodo da Bisseção:----------- \n "<< bisection(func1,0,1,pow(10,-4),0) << "\n";
 	std::cout << "\nMétodo da Posição Falsa:------ \n" << falsepoint(func1,0,1,pow(10,-4),0) << "\n";
 	//std::cin >> input;std::cout << func1(input) << "\n";std::cout<<derivative_func1(input)<<"\n"; 
